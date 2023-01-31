56eeb45656053c166ac35df9f47ccde3c78dec63
maksim <mkozharin@gmail.com>
2023-01-31 11:59:42 +0300

    Решение задачи № 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+int rez = 0;
+int rez1 = 0;
+Console.WriteLine("Введите любое число");
+string? input = Console.ReadLine();
+bool result = int.TryParse(input, out var number);
+if (result == true)
+    {
+       while(number > 9)
+           {
+                rez1 = number % 10;
+                rez = rez + rez1;
+                number = number / 10; 
+            }
+        rez = rez + number;
+        Console.WriteLine(rez);    
+    }
+else
+    {
+        Console.WriteLine("Вы ввели не число");   
+    }
+
